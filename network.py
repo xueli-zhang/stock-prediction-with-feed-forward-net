d72c0707a8a3f281f2596a6ede06af7a8140d9cf
Xueli-Zhang <xueli.zhang@carleton.ca>
2018-04-11 22:25:54 -0400

    finished feed_forward and backpropergation

--- network.py
+++ network.py
@@ -4,15 +4,18 @@ import random
 
 class feed_forward_net(object):
 	
-	def __init__(self, input_size, hidden_layers_size, output_size):
+	def __init__(self, input_size, hidden_layers_size, output_size, learning_rate = 0.5):
 
 		#create input layers
 		self.input_layer = np.zeros(input_size, dtype = float)
 		self.input_layer = np.append(self.input_layer, [-1])
+		self.learning_rate = learning_rate
 
 		self.weights = []
 		hidden_layers_size = [x+1 for x in hidden_layers_size]
 		prev_layer_size =np.append([input_size+1], hidden_layers_size)
+
+		self.errors = []
 
 
 		#create hidden layers
@@ -23,8 +26,8 @@ class feed_forward_net(object):
 				for j in range(y):
 					for k in range(x):
 						layer_weight[j][k] = random.uniform(0, 1)
-				print("layer weight: ")
-				print(layer_weight)
+				#print("layer weight: ")
+				#print(layer_weight)
 				self.weights.append(layer_weight)
 
 		#create output layers
@@ -34,11 +37,20 @@ class feed_forward_net(object):
 		for j in range(y):
 			for k in range(x):
 				layer_weight[j][k] = random.uniform(0, 1)
-		print("layer weight: ")
-		print(layer_weight)
+		#print("layer weight: ")
+		#print(layer_weight)
 		self.weights.append(layer_weight)
 
 		self.output_layer = np.zeros(output_size, dtype = float)
+
+		print("input layer: ")
+		print(self.input_layer)
+
+		print("weights: ")
+		print(self.weights)
+
+		print("output layer: ")
+		print(self.output_layer)
 
 
 
@@ -46,7 +58,9 @@ class feed_forward_net(object):
 
 		print("inputs: ")
 		print(inputs)
+		self.activations = []
 		outputs = self.input_layer
+
 
 		if len(inputs)+1 != len(self.input_layer):
 			print("feeding inputs demsion not matching input layer demsion...")
@@ -58,47 +72,87 @@ class feed_forward_net(object):
 		for layer_weight in self.weights:
 			outputs = np.dot(outputs, layer_weight)
 			outputs = self.sigmoid(outputs)
-			print("outputs: ")
-			print(outputs)
-		print("output_layer before: ")
-		print(self.output_layer)
+			self.activations.append(outputs.reshape(len(outputs), 1))
+			#print("outputs: ")
+			#print(outputs)
+		self.activations = self.activations[:-1]
+		#print("activations: ")
+		#print(self.activations)
+		#print("output_layer before: ")
+		#print(self.output_layer)
 		for i in range(len(outputs)):
 			self.output_layer[i] = outputs[i]
-		print("output_layer: ")
-		print(self.output_layer)
+		#print("output_layer: ")
+		#print(self.output_layer)
 
 
 	def backpropagation(self, labels):
 
 		labels = np.divide(labels, 10000).reshape(len(labels), 1)
-		print("labels: ")
-		print(labels)
+		#print("labels: ")
+		#print(labels)
 
 		outputs = self.output_layer.reshape(len(self.output_layer), 1)
-		print("outputs: ")
-		print(outputs)
-		print("outputs 1:")
-		print(self.output_layer)
+		#print("outputs: ")
+		#print(outputs)
+		#print("outputs 1:")
+		#print(self.output_layer)
 
 		last_neuron_error = np.subtract(labels, outputs)
-		print("last_neuron_error: ")
-		print(last_neuron_error)
+		#print("last_neuron_error: ")
+		#print(last_neuron_error)
+
+		self.errors.append(last_neuron_error)
 
 		last_neuron_correction = np.subtract(1, outputs)
-		print("last_neuron_correction: ")
-		print(last_neuron_correction)
+		#print("last_neuron_correction: ")
+		#print(last_neuron_correction)
 
-		
+		layer_corrections = []
 
 
+		layer_corrections.append(last_neuron_correction)
+		#print("layer corrections: ")
+		#print(layer_corrections)
+
+		for i in reversed(range(len(self.weights))):
+			if i != 0:
+				new_correction = np.multiply(np.multiply(self.activations[i-1], np.subtract(1, self.activations[i-1])),
+					np.dot(self.weights[i], layer_corrections[len(self.weights)-1-i]))
+				layer_corrections.append(new_correction)
+
+		layer_corrections = layer_corrections[::-1]
+		print("layer corrections: ")
+		print(layer_corrections)
+
+		#update weights
+		input_and_hidden = []
+		input_and_hidden.append(self.input_layer.reshape(len(self.input_layer), 1))
+
+		for i in self.activations:
+			input_and_hidden.append(i)
+
+
+		print("input_and_hidden: ")
+		print(input_and_hidden)
+
+		for i in range(len(input_and_hidden)):
+
+			for j in range(len(input_and_hidden[i])):
+
+				for k in range(len(layer_corrections[i])):
+
+					self.weights[i][j][k] += self.learning_rate * input_and_hidden[i][j][0] * layer_corrections[i][k][0]
+
+		print("new weights:")
+		print(self.weights)
 
 	def sigmoid(self, inputs):
 		return 1 / (1 + np.exp(-inputs))
 
 
+	def train(self, train_dataset, )
 
-	def sigmoid_p(self, inputs):
-		return self.sigmoid(inputs)*(1 - self.sigmoid(inputs))
 
 
 def main():
